8f80af594b5cf0ed7d81607dd08b881e72c17ef2
Matias Sio <siomatias20@gmail.com>
2025-01-27 11:56:08 -0300

    First commit

---
+++ src/layouts/default.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@heroui/link";
+
+import { Provider } from "../provider";
+
+import { Navbar } from "@/components/navbar";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex flex-col h-screen">
+      <Navbar />
+      <main className="container flex-grow px-6 pt-16 mx-auto max-w-7xl">
+        <Provider>{children}</Provider>
+      </main>
+      <footer className="flex items-center justify-center w-full py-3">
+        <Link
+          isExternal
+          className="flex items-center gap-1 text-current"
+          href="https://heroui.com"
+          title="heroui.com homepage"
+        >
+          <span className="text-default-600">Powered by</span>
+          <p className="text-primary">HeroUI</p>
+        </Link>
+      </footer>
+    </div>
+  );
+}
